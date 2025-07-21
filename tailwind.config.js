af6f0ae865ac79c47b9be5b2c1a219bcd0aa9ec1
IT-DEV <it-dev@IT-DEVs-MacBook-Air.local>
2025-07-21 16:32:40 +0700

    Add custom background image to Tailwind CSS config

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ module.exports = {
     "./src/**/*.{js,jsx}"
   ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'food-bg': "url('https://images.unsplash.com/photo-1636391886989-da35ee18cbdb?q=80&w=987&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')",
+      },
+    },
   },
   plugins: [],
 }
