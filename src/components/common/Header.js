8e53aaa2d8a4f13d9b32f315c2c572636e1ac119
keosmlao <keoitc2014@gmail.com>
2025-07-25 15:31:16 +0700

    feat: update header text and color

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -4,10 +4,10 @@ const Header = ({ userId }) => {
   return (
     <header className="mb-8 text-center drop-shadow-xl">
       <img src="/FoodODG.png" alt="FoodODG Logo" className="mx-auto mb-4" style={{ maxWidth: '150px' }} />
-      <h1 className="mb-2 text-5xl font-extrabold text-teal-800 drop-shadow-lg">
-        ODIEN canteen
+      <h1 className="mb-2 text-5xl font-extrabold text-black drop-shadow-lg">
+        ODIEN COOKING
       </h1>
-      <p className="text-xl text-teal-600">ຈັດການລາຍການເມນູອາຫານທີ່ທ່ານມັກ ແລະ ໂຫວດເມນູປະຈຳວັນ</p>
+      <p className="text-xl text-gray-800">ODIEN COOKING</p>
       <p className="text-sm text-gray-500 mt-2">ລະຫັດຜູ້ໃຊ້: {userId}</p>
     </header>
   );
