33b7850b966ac5bd24c6d028fccd7ebee8d7cc8c
keosmlao <keoitc2014@gmail.com>
2025-07-25 15:03:44 +0700

    Update background image to use local BG.png

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,7 @@ module.exports = {
   theme: {
     extend: {
       backgroundImage: {
-        'food-bg': "url('https://images.unsplash.com/photo-1636391886989-da35ee18cbdb?q=80&w=987&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')",
+        'food-bg': "url('/BG.png')",
       },
     },
   },
