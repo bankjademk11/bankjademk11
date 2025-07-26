7211df30fba1cf9a2fd05ed030c18ee9b6829df2
keosmlao <keoitc2014@gmail.com>
2025-07-26 09:45:04 +0700

    Revert "feat: add translucent card to header and update subtitle"
    This reverts commit 28c482b26566c9d27ca6393f32b459b2cdcc1866.

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -4,12 +4,10 @@ const Header = ({ userId }) => {
   return (
     <header className="mb-8 text-center drop-shadow-xl">
       <img src="/FoodODG.png" alt="FoodODG Logo" className="mx-auto mb-4" style={{ maxWidth: '150px' }} />
-      <div className="bg-gray-100 bg-opacity-70 p-4 rounded-lg shadow-md mb-4">
-        <h1 className="mb-2 text-5xl font-extrabold text-blue-600 drop-shadow-lg font-playfair">
-          ODIEN COOKING
-        </h1>
-        <p className="text-xl text-gray-700">Enjoy ກັບອາຫານຂອງທ່ານ</p>
-      </div>
+      <h1 className="mb-2 text-5xl font-extrabold text-blue-600 drop-shadow-lg font-playfair">
+        ODIEN COOKING
+      </h1>
+      <p className="text-xl text-gray-800">ODIEN COOKING</p>
       <p className="text-sm text-gray-500 mt-2">ລະຫັດຜູ້ໃຊ້: {userId}</p>
     </header>
   );
