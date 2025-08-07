120ca6e73e989bd2c07db57f8e3fba1ceb7f9415
keosmlao <keoitc2014@gmail.com>
2025-08-07 10:54:12 +0700

    fix: Resolve RootHandler import warning
    - Removed unused AuthRequiredMessage import from RootHandler.js.

--- src/pages/AuthGate.js
+++ src/pages/AuthGate.js
@@ -1,52 +1,19 @@
-import { useEffect } from 'react';
-import { useLocation } from 'react-router-dom';
+const getAuthStatusFromUrl = (location) => {
+  const urlParams = new URLSearchParams(location.search);
+  const userIdFromUrl = urlParams.get('userID');
 
-const AuthGate = () => {
-  const location = useLocation();
+  let userId = null;
+  let errorType = null;
 
-  useEffect(() => {
-    const urlParams = new URLSearchParams(location.search);
-    const userIdFromUrl = urlParams.get('userID'); // Check for userID in URL
-    console.log('AuthGate: userIdFromUrl', userIdFromUrl);
-    let storedUserId = localStorage.getItem('offlineUserId'); // Check for userID in localStorage
-    console.log('AuthGate: storedUserId', storedUserId);
-
-    let finalUserId = null;
-    let errorType = null;
-
-    if (userIdFromUrl) {
-      if (userIdFromUrl.toLowerCase() === 'emtry') {
-        errorType = 'invalid_userid';
-        console.log('AuthGate: userIdFromUrl is Emtry');
-      } else {
-        finalUserId = userIdFromUrl;
-        console.log('AuthGate: finalUserId from URL', finalUserId);
-      }
-    } else if (storedUserId) {
-      finalUserId = storedUserId;
-      console.log('AuthGate: finalUserId from stored', finalUserId);
-    }
-
-    if (finalUserId) {
-      console.log('AuthGate: Setting offlineUserId in localStorage', finalUserId);
-      localStorage.setItem('offlineUserId', finalUserId);
-      console.log('AuthGate: Dispatching userLoggedIn event');
-      window.dispatchEvent(new Event('userLoggedIn')); // Notify App.js
+  if (userIdFromUrl) {
+    if (userIdFromUrl.toLowerCase() === 'emtry') {
+      errorType = 'invalid_userid';
     } else {
-      // If no userId from URL and no stored userId, or if userIdFromUrl was 'Emtry'
-      if (!errorType) {
-        errorType = 'no_userid';
-      }
-      // Set error in localStorage for App.js to pick up
-      localStorage.setItem('authError', errorType);
-      console.log('AuthGate: Setting authError in localStorage', errorType);
-      window.dispatchEvent(new Event('authErrorOccurred')); // Notify App.js
+      userId = userIdFromUrl;
     }
-  }, [location]);
+  }
 
-  // This component doesn't render anything if it's redirecting
-  // If it's displaying an error, App.js will handle it based on URL param
-  return null;
+  return { userId, errorType };
 };
 
-export default AuthGate;
+export default getAuthStatusFromUrl;
