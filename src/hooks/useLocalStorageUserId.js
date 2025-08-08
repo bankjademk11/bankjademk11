fea3837556b7d714e29ebb66a0e8996ee5b78aeb
keosmlao <keoitc2014@gmail.com>
2025-08-08 09:51:46 +0700

    fix: Remove unused variable to pass CI build

--- src/hooks/useLocalStorageUserId.js
+++ src/hooks/useLocalStorageUserId.js
@@ -3,7 +3,7 @@ import { useState, useEffect } from 'react';
 const useLocalStorageUserId = () => {
   // This hook no longer interacts with localStorage.
   // It will be updated later to receive the userId from the new auth system.
-  const [userId, setUserId] = useState(null);
+  const [userId] = useState(null);
 
   useEffect(() => {
     // The logic for handling storage changes is removed for now.
